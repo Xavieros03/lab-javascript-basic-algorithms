511eecf03f36800ab0720af345e7024e5f790a7c
Xavier Ortega <xaviersendra@hotmail.com>
2023-05-12 20:49:37 +0200

    bonus1 done

--- index.js
+++ index.js
@@ -42,3 +42,26 @@ else if(hacker1.localeCompare(hacker2) === 1){
 else if(hacker1.localeCompare(hacker2) === 0){
   console.log("What?! You both have the same name?");
 }
+
+//Bonus 1
+
+let longText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras lobortis nibh vel diam rhoncus auctor. Donec enim ante, suscipit nec gravida nec, molestie nec odio. Morbi sed elementum velit, vel lacinia ante. Sed imperdiet egestas tempor. Cras vitae ligula ex. Quisque eleifend nunc sed est ullamcorper, a dictum risus eleifend. Sed in facilisis enim, vitae pellentesque neque. Proin posuere sodales malesuada. Nam commodo mattis ante at mollis. Ut posuere, ipsum vel tincidunt pulvinar, arcu tellus pretium neque, at vehicula erat turpis eget dui. Vestibulum tempor finibus ornare. Morbi consequat leo sed massa dictum pretium. Mauris aliquam orci non volutpat imperdiet. Proin sodales efficitur ultrices. Cras commodo ac felis vitae molestie.
+
+Curabitur faucibus, metus eget rutrum molestie, lacus erat aliquet tellus, eu scelerisque sapien lacus nec leo. Nunc eget vestibulum urna, id convallis mi. Nullam condimentum augue laoreet, suscipit turpis ut, vulputate sem. Etiam vel tortor laoreet, laoreet mi non, aliquet enim. Nunc ac accumsan eros. Phasellus id luctus est, eu sagittis mi. Suspendisse interdum nulla ante, a interdum ex maximus euismod. Nam ullamcorper nisl eu ipsum tempus molestie id a est. Pellentesque id fermentum velit, nec viverra elit. Sed sapien neque, facilisis sit amet pretium ut, convallis et lacus. Nulla vel justo at lectus ornare efficitur nec id ex. Suspendisse quis orci sagittis metus placerat hendrerit. Aenean vehicula semper velit at malesuada. Praesent eget mollis nisi. Mauris nunc libero, pulvinar at mi nec, rhoncus ultricies orci. Phasellus eu massa id tortor sagittis pulvinar.
+
+Praesent tempor eros in sapien ullamcorper fringilla. Praesent eros massa, cursus eu sapien vel, volutpat suscipit dolor. Vestibulum non diam sem. Quisque rhoncus, est in rhoncus porttitor, tellus libero tristique urna, id fermentum neque eros eu felis. Fusce nulla nisi, congue ut justo sed, tristique tempor ligula. Vestibulum rutrum nec leo eu tincidunt. Aliquam elementum est ex, eu blandit justo varius et. Vestibulum a sem lectus. Aliquam erat volutpat. Suspendisse egestas laoreet tincidunt. Sed mattis quis purus ut fringilla. Aenean faucibus felis ac mattis iaculis. Mauris convallis, lorem vel finibus iaculis, risus sapien bibendum massa, et molestie lectus nisl vel ex."
+let countWords = 1
+for (let u=0; u < longText.length; u++){
+  if (longText[u] === " "){
+    countWords+= 1
+  }
+}
+console.log(countWords)
+
+let countEt = 1
+for (let e=0; e < longText.length; e++){
+  if (longText[e] === "e" && longText[e+1] === "t"){
+    countEt+= 1
+  }
+}
+console.log(countEt)
